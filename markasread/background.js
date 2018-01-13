7c6145637fb7f184bfba3953393b383e02ac91b5
Francisco Javier Martinez Lago <edoreld@gmail.com>
2018-01-13 15:21:14 +0100

    fixed extension not working when there were multiple windows open

--- markasread/background.js
+++ markasread/background.js
@@ -1,18 +1,18 @@
 chrome.runtime.onInstalled.addListener(function () {
-	// console.log("onInstalled");
+	console.log("onInstalled");
 	chrome.storage.sync.get("visited", function (obj) {
 		if (obj["visited"] == undefined) {
-			// console.log("obj undefined");
+			console.log("obj undefined");
 			visited = {};
 		} else {
-			// console.log("obj defined");
+			console.log("obj defined");
 			visited = obj["visited"];
 		}
 	});
 })
 
 chrome.runtime.onStartup.addListener(function () {
-	// console.log("onStartup");
+	console.log("onStartup");
 	visited = {};
 	chrome.storage.sync.get("visited", function (obj) {
 		if (obj["visited"] == undefined) {
@@ -23,37 +23,51 @@ chrome.runtime.onStartup.addListener(function () {
 	});
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) { 
-	// console.log("onClicked");
-	chrome.tabs.getSelected(null, function(tab){
-		if (visited[tab.url] == false || visited[tab.url] == undefined) {
-			markAsVisited();
-			visited[tab.url] = true;
+// chrome.browserAction.onClicked.addListener(function(tab) { 
+// 	console.log("onClicked");
+// 	chrome.tabs.getSelected(null, function(tab){
+// 		if (visited[tab.url] == false || visited[tab.url] == undefined) {
+// 			markAsVisited();
+// 			visited[tab.url] = true;
 			
-		} else {
-			markAsNotVisited();
-			visited[tab.url] = false;
+// 		} else {
+// 			markAsNotVisited();
+// 			visited[tab.url] = false;
+// 		}
+// 	});
+// });
+
+chrome.browserAction.onClicked.addListener(function(tabs) { 
+	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
+		console.log(tab[0].url);
+		if (visited[tab[0].url] == undefined || visited[tab[0].url] == false) {
+			visited[tab[0].url] = true;
+			markAsVisited(tab[0].id);
+		} else { 
+			visited[tab[0].url] = false;
+			markAsNotVisited(tab[0].id);
 		}
 	});
-});
+})
 
 /** 
 * Upon switching to a new tab and on it being activated, we check if this is the tab's
 * first time being loaded, and if so we mark it as not visited
 */
 chrome.tabs.onActivated.addListener(function callback(activeInfo) {
-	// console.log("onActivated");
-	chrome.tabs.getSelected(null, function(tab){
-		if (visited[tab.url] == undefined || visited[tab.url] == false) {
-			markAsNotVisited();
+	console.log("onActivated");
+	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
+		console.log(tab[0].url);
+		if (visited[tab[0].url] == undefined || visited[tab[0].url] == false) {
+			markAsNotVisited(tab[0].id);
 		} else { 
-			markAsVisited();
+			markAsVisited(tab[0].id);
 		}
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function callback(activeInfo) {
-	// console.log("onActivated");
+	console.log("onActivated");
 	chrome.tabs.getSelected(null, function(tab){
 		if (visited[tab.url] == undefined || visited[tab.url] == false) {
 			markAsNotVisited();
@@ -63,30 +77,31 @@ chrome.tabs.onUpdated.addListener(function callback(activeInfo) {
 	});
 });
 
+
 function updateRemoteDictionary() {	
 	chrome.storage.sync.set({"visited": visited}, function() {
 		if (chrome.runtime.error) {
-			// console.log("Runtime error.");
+			console.log("Runtime error.");
 		}
 	});
 }
 
-function markAsNotVisited() {
-	// console.log("markAsNotVisited");
-	chrome.browserAction.setIcon({path: "notvisited.png"});
+function markAsNotVisited(atabId) {
+	console.log("markAsNotVisited");
+	chrome.browserAction.setIcon({path: "notvisited.png", tabId: atabId});
 	updateRemoteDictionary();
 }
 
-function markAsVisited() {
-	// console.log("markAsVisited");
-	chrome.browserAction.setIcon({path: "visited.png"});
+function markAsVisited(atabId) {
+	console.log("markAsVisited");
+	chrome.browserAction.setIcon({path: "visited.png", tabId: atabId });
 	updateRemoteDictionary();
 }
 
 // chrome.storage.onChanged.addListener(function(changes, namespace) {
 // 	for (key in changes) {
 // 		var storageChange = changes[key];
-// 		// console.log('Storage key "%s" in namespace "%s" changed. ' +
+// 		console.log('Storage key "%s" in namespace "%s" changed. ' +
 // 			'Old value was "%s", new value is "%s".',
 // 			key,
 // 			namespace,
