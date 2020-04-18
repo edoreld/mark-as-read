0a90757e37e98fe72cbfe6b5c6ce442a6acf4f76
Helcio Silva <helciodasilva@gmail.com>
2020-04-18 09:55:45 -0300

    Extract Method

--- markasread/options.js
+++ markasread/options.js
@@ -1,28 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	let download = document.getElementById('download');
-	download.onclick = function(element) {
-		chrome.storage.sync.get("visited", function (obj) {
+function download() {
+	chrome.storage.sync.get("visited", function (obj) {
 		var result = JSON.stringify(obj);
 		var url = 'data:application/json;base64,' + btoa(result);
 		chrome.downloads.download({
 			url: url,
 			filename: 'data.json'
 		});
-		});
-	};	
+	});
+}
 
-	let upload = document.getElementById('upload');
-	upload.addEventListener("change", function () {
-		var file = this.files[0];
-		var reader = new FileReader();
-		reader.onload = function(e){
-			var result = JSON.parse(e.target.result);
-			console.log("result:" + result.visited);
-			chrome.runtime.sendMessage({action: 'import', data: result});
-		}
-		reader.readAsText(file);
-		upload.value = '';
-	}, false);	 
-	
+function upload() {
+	var file = this.files[0];
+	var reader = new FileReader();
+	reader.onload = function(e){
+		var result = JSON.parse(e.target.result);
+		console.log("result:" + result.visited);
+		chrome.runtime.sendMessage({action: 'import', data: result});
+	}
+	reader.readAsText(file);
+	upload.value = '';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("download").addEventListener("click", download);
+	document.getElementById('upload').addEventListener("change", upload, false);	
 }, false);
