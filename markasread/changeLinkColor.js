acae7e55f6784f62fcef3b6cdf63801430e23edd
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2022-05-07 21:07:31 -0600

    Change color of all text related to an achor.

--- markasread/changeLinkColor.js
+++ markasread/changeLinkColor.js
@@ -1,19 +1,16 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action == "get_links") {
-        var links = document.querySelectorAll('a');
+        let links = document.querySelectorAll('a');
         links = Array.from(links).map(link => link.href)
         sendResponse(links)
     } else if (message.action == "change_link_color") {
-        var links = document.querySelectorAll('a');
+        let links = document.querySelectorAll('a');
         const linksSet = new Set(message.links)
         links.forEach(link => {
             if (linksSet.has(link.href)) {
-                link.style.color = message.linkColor;
+                 link.querySelectorAll("*").forEach(a => a.style.color = message.linkColor)
             }
         })
         sendResponse()
     }
 });
-
-
-// main()
